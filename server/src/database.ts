2ab9f8f5bc8c43477d5e1693eda0f425c65fc6a0
MateusJSouza <mateusjdev@gmail.com>
2024-09-12 17:35:06 -0300

    feat: adicionar configuração de conexão com o banco de dados

---
+++ server/src/database.ts
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize";
+import path from "path";
+
+const config = require(path.join(__dirname, "config", "config.json"))[
+	"development"
+];
+
+export const sequelize = new Sequelize(
+	config.database,
+	config.username,
+	config.password,
+	{
+		host: config.host,
+		port: config.port,
+		dialect: "postgres",
+		logging: false,
+	},
+);
+
+export async function connectToDatabase() {
+	try {
+		await sequelize.authenticate();
+		console.log("Conexão com o banco de dados estabelecida com sucesso.");
+	} catch (error) {
+		console.error("Não foi possível conectar ao banco de dados:", error);
+	}
+}
