379c549b86f289505e0406723bc4aa7e24bb0d01
Evandro Calado <evandrocalado07@gmail.com>
2024-09-14 20:04:28 -0300

    🐞 fix: remove value and on change

--- frontend/src/components/TextField/TextField.tsx
+++ frontend/src/components/TextField/TextField.tsx
@@ -1,40 +1,22 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 
 export type TextFieldProps = {
   label?: string;
-  initialValue?: string;
-  onInput?: (value: string) => void;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-export const TextField: FC<TextFieldProps> = ({
-  label,
-  initialValue,
-  onInput,
-  ...rest
-}) => {
-  const [value, setValue] = useState("");
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value;
-    setValue(newValue);
-
-    onInput?.(newValue);
-  };
-
+export const TextField: FC<TextFieldProps> = ({ label, ...rest }) => {
   return (
     <div>
       <label
         htmlFor={label}
-        className="block mb-2 w-max text-sm font-medium text-gray-900 cursor-pointer"
+        className="block mb-2 w-max text-sm font-medium cursor-pointer"
       >
         {label}
       </label>
       <input
         id={label}
         {...rest}
-        value={initialValue || value}
-        onChange={onChange}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:border-blue-500 focus:ring-1 focus:ring-blue-500 block w-full p-2.5 outline-none disabled:bg-gray-200 disabled:pointer-events-none"
+        className="bg-gray-700 border border-gray-600 text-gray-50 text-sm rounded-lg hover:border-blue-500 focus:ring-1 focus:ring-blue-500 block w-full p-2.5 outline-none disabled:bg-gray-200 disabled:pointer-events-none"
       />
     </div>
   );
