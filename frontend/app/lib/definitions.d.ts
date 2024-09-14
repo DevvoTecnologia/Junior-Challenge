43c72d6b7864735dc72a9873670a29f58cb25418
João Vítor Ferraz <git@p1pa.dev>
2024-09-14 01:51:51 -0300

    feat: create ts definitions for fe

---
+++ frontend/app/lib/definitions.d.ts
@@ -0,0 +1,19 @@
+export interface Ring {
+  id: number;
+  name: string;
+  power: string;
+  forgedBy: 'Elfos' | 'Anões' | 'Humanos' | 'Sauron';
+  image: string;
+}
+
+export interface Owner {
+  id: number;
+  name: string;
+}
+
+export type RawRingData = Omit<Ring, 'id'> & { ownerName: string };
+
+export type APIRingData = {
+  ring: Omit<Ring, 'id'>;
+  owner: Omit<Owner, 'id'>;
+};
