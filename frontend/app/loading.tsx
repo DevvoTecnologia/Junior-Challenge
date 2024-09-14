a9428b12fed37ac894955335146c4367c07e46d3
João Vítor Ferraz <git@p1pa.dev>
2024-09-14 01:52:45 -0300

    feat: create loading suspense page

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,9 @@
+import styles from './loading.module.css';
+
+export default function Loading() {
+  return (
+    <article className={styles.loading}>
+      <span aria-busy="true">Carregando os anéis...</span>
+    </article>
+  );
+}
