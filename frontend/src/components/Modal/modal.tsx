b30fbd4f211e8b5cebde2557880654344405a797
Evandro Calado <evandrocalado07@gmail.com>
2024-09-14 20:03:39 -0300

    🐳 chore: add set show modal

--- frontend/src/components/Modal/modal.tsx
+++ frontend/src/components/Modal/modal.tsx
@@ -5,12 +5,14 @@ export type ModalProps = {
   title: string;
   children: React.ReactNode;
   showModal?: boolean;
+  setShowModal?: (value: boolean) => void;
 };
 
 export const Modal: FC<ModalProps> = ({
   title,
   children,
   showModal = false,
+  setShowModal,
 }) => {
   const [isOpen, setIsOpen] = useState(showModal);
 
@@ -24,18 +26,20 @@ export const Modal: FC<ModalProps> = ({
         isOpen
           ? "visible opacity-100"
           : "invisible opacity-0 pointer-events-none"
-      } absolute transition-all duration-300 inset-0 flex items-center justify-center bg-zinc-800/30`}
+      } absolute transition-all duration-300 inset-0 flex items-center justify-center bg-gray-800/80`}
       aria-hidden={!isOpen}
     >
-      <div className="relative p-8 bg-zinc-800 rounded-lg border-zinc-300">
+      <div className="relative p-8 bg-gray-700 rounded-lg border-gray-600 border w-[500px]">
         <IconX
-          className="absolute right-2 top-2 cursor-pointer text-white hover:text-zinc-400 duration-150"
+          className="absolute right-4 top-4 cursor-pointer text-white hover:text-gray-400 duration-150"
           aria-label="Close Modal"
           size={20}
-          onClick={() => setIsOpen(false)}
+          onClick={() => setShowModal?.(false)}
         />
-        <h2 className="text-white text-2xl font-semibold">{title}</h2>
-        <hr className="my-4 border-zinc-500 -mx-8" />
+        <h2 className="text-white text-2xl font-semibold text-center">
+          {title}
+        </h2>
+        <hr className="my-4 border-gray-600 -mx-8" />
         {children}
       </div>
     </div>
