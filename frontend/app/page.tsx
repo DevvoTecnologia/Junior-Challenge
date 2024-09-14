852238bed3de7e5c440c58fe4e9f1a2cc0b0fa39
João Vítor Ferraz <git@p1pa.dev>
2024-09-14 16:32:07 -0300

    feat: use api function

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,34 +1,17 @@
-import type { Ring } from '@/app/lib/definitions';
+import getRings from '@/app/api/getRings';
 import styles from '@/app/page.module.css';
 import RingsCarousel from '@/app/ui/slick/rings-carousel';
 
-async function getRings() {
-  try {
-    const res = await fetch('http://localhost:3000/rings', {
-      next: {
-        tags: ['rings'],
-      },
-    });
-
-    if (!res.ok) {
-      return null;
-    }
-
-    return res.json();
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-
 export default async function Home() {
-  const rings: Ring[] = await getRings();
+  const rings = await getRings();
   return (
-    <div className={styles.container + ' container-fluid'}>
-      <h1 className={styles.title}>Anéis no mundo</h1>
-      <hr />
-      {rings && <RingsCarousel rings={rings} />}
-      {!rings && <p className="text-center">Nenhum anel encontrado :(</p>}
-    </div>
+    <>
+      <div className={styles.container + ' container-fluid'}>
+        <h1 className={styles.title}>Anéis no mundo</h1>
+        <hr />
+        {rings && <RingsCarousel rings={rings} />}
+        {!rings && <p className="text-center">Nenhum anel encontrado :(</p>}
+      </div>
+    </>
   );
 }
