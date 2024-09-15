474fc066a6c8ce5de568a4b960af39eb8d781bc0
Evandro Calado <evandrocalado07@gmail.com>
2024-09-15 17:02:52 -0300

    🧪 test: fix tests

--- frontend/src/components/Form/Form.test.tsx
+++ frontend/src/components/Form/Form.test.tsx
@@ -1,64 +1,88 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Form } from "./Form";
 
-describe("<Form />", () => {
-  it("should render form by default", () => {
-    render(<Form />);
+const initialValues = {
+  _id: "1",
+  name: "One Ring",
+  power: "Invisibility",
+  holder: "Frodo",
+  forgedBy: "Sauron",
+  image: "http://example.com/image.jpg",
+};
 
-    expect(screen.getByRole("textbox", { name: "Nome" })).toBeInTheDocument();
-    expect(screen.getByRole("textbox", { name: "Nome" })).toHaveValue("");
-    expect(screen.getByRole("textbox", { name: "Poder" })).toBeInTheDocument();
-    expect(
-      screen.getByRole("textbox", { name: "Portador" })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("textbox", { name: "Forjado por" })
-    ).toBeInTheDocument();
-    expect(screen.getByRole("textbox", { name: "Imagem" })).toBeInTheDocument();
-    expect(screen.getByRole("button")).toBeInTheDocument();
+describe("<Form />", () => {
+  beforeAll(() => {
+    HTMLFormElement.prototype.requestSubmit = vi.fn();
   });
 
-  it("should render with initial values", () => {
-    const initialValues = {
-      name: "name",
-      power: "power",
-      holder: "holder",
-      forgedBy: "forgedBy",
-      image: "image",
-    };
-
+  it("should render form with initial values", () => {
     render(<Form initialValues={initialValues} />);
 
-    expect(screen.getByRole("textbox", { name: "Nome" })).toHaveValue(
-      initialValues.name
-    );
-    expect(screen.getByRole("textbox", { name: "Poder" })).toHaveValue(
-      initialValues.power
-    );
-    expect(screen.getByRole("textbox", { name: "Portador" })).toHaveValue(
+    expect(screen.getByLabelText(/nome/i)).toHaveValue(initialValues.name);
+    expect(screen.getByLabelText(/poder/i)).toHaveValue(initialValues.power);
+    expect(screen.getByLabelText(/portador/i)).toHaveValue(
       initialValues.holder
     );
-    expect(screen.getByRole("textbox", { name: "Forjado por" })).toHaveValue(
+    expect(screen.getByLabelText(/forjado por/i)).toHaveValue(
       initialValues.forgedBy
     );
-    expect(screen.getByRole("textbox", { name: "Imagem" })).toHaveValue(
-      initialValues.image
+    expect(screen.getByLabelText(/imagem/i)).toHaveValue(initialValues.image);
+  });
+
+  it("should update input when type", async () => {
+    const handleSubmit = vi.fn();
+    const handleChange = vi.fn();
+
+    render(
+      <Form
+        initialValues={initialValues}
+        onDataChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    );
+
+    expect(screen.getByLabelText(/nome/i)).toHaveValue(initialValues.name);
+    expect(screen.getByLabelText(/poder/i)).toHaveValue(initialValues.power);
+    expect(screen.getByLabelText(/portador/i)).toHaveValue(
+      initialValues.holder
+    );
+    expect(screen.getByLabelText(/forjado por/i)).toHaveValue(
+      initialValues.forgedBy
+    );
+    expect(screen.getByLabelText(/imagem/i)).toHaveValue(initialValues.image);
+
+    const inputName = screen.getByLabelText(/nome/i);
+
+    await userEvent.clear(inputName);
+
+    await userEvent.type(inputName, "new name");
+
+    expect(handleChange).toHaveBeenCalled();
+    expect(handleChange).toHaveBeenCalledWith(
+      expect.objectContaining({
+        name: "new name",
+      })
     );
   });
 
-  it("should onFormSubmit called on submit", () => {
-    const onFormSubmit = vi.fn();
+  it("should handle sibmit form", async () => {
+    const handleSubmit = vi.fn();
 
-    render(<Form onFormSubmit={onFormSubmit} />);
+    render(<Form initialValues={initialValues} handleSubmit={handleSubmit} />);
 
-    const nameInput = screen.getByRole("textbox", { name: "Nome" });
+    const button = screen.getByRole("button", { name: "Salvar" });
+
+    await userEvent.click(button);
+
+    expect(handleSubmit).toHaveBeenCalled();
+  });
+
+  it("should render loading text", () => {
+    render(<Form initialValues={initialValues} loading />);
+
     const button = screen.getByRole("button");
 
-    userEvent.type(nameInput, "test");
-
-    fireEvent.click(button);
-
-    expect(onFormSubmit).toHaveBeenCalledTimes(1);
+    expect(button).toHaveTextContent("Salvando...");
   });
 });
