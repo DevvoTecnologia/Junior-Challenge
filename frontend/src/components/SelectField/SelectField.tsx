9abef1cbe969ad100c5e8297f03eacc9df0f462a
Evandro Calado <evandrocalado07@gmail.com>
2024-09-14 23:44:41 -0300

    🧪 test: add test

--- frontend/src/components/SelectField/SelectField.tsx
+++ frontend/src/components/SelectField/SelectField.tsx
@@ -3,8 +3,8 @@ import { FC } from "react";
 export type SelectFieldProps = {
   name: string;
   label: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   options: { value: string; label: string }[];
 } & React.SelectHTMLAttributes<HTMLSelectElement>;
 
@@ -25,6 +25,7 @@ export const SelectField: FC<SelectFieldProps> = ({
         name={name}
         value={value}
         onChange={onChange}
+        data-testid="select-element"
         className="block w-full pl-3 pr-10 py-[12px] text-base bg-gray-700 border-gray-600 focus:outline-none border duration-150 hover:border-blue-500 focus:ring-blue-500  sm:text-sm rounded-lg"
       >
         {options.map((option) => (
