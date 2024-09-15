30c141d22448f3434e6e0955b9d10956b4574002
Evandro Calado <evandrocalado07@gmail.com>
2024-09-15 17:03:03 -0300

    🧪 test: fix tests

--- frontend/src/components/Modal/Modal.test.tsx
+++ frontend/src/components/Modal/Modal.test.tsx
@@ -1,32 +1,50 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { Modal } from "./modal";
 
+vi.mock("framer-motion", () => ({
+  motion: {
+    div: vi.fn(({ children, ...rest }) => <div {...rest}>{children}</div>),
+  },
+  AnimatePresence: vi.fn(({ children }) => <>{children}</>),
+}));
+
 describe("<Modal />", () => {
-  it("should render modal hidden", () => {
-    const { container } = render(<Modal title="test">content</Modal>);
-
-    expect(container.firstChild).toHaveClass(
-      "invisible opacity-0 pointer-events-none"
-    );
-    expect(screen.getByText("test")).toBeInTheDocument();
-    expect(screen.getByText("content")).toBeInTheDocument();
-  });
-
-  it("should render open/close modal", () => {
-    const { container } = render(
-      <Modal title="test" showModal>
+  it("should render modal visible", () => {
+    render(
+      <Modal title="test" showModal setShowModal={vi.fn()}>
         content
       </Modal>
     );
 
-    expect(container.firstChild).toHaveClass("visible opacity-100");
+    expect(screen.getByText("test")).toBeInTheDocument();
+    expect(screen.getByText("content")).toBeInTheDocument();
+    expect(screen.getByLabelText("Close Modal")).toBeInTheDocument();
+  });
 
-    const closeButton = screen.getByLabelText("Close Modal");
-
-    fireEvent.click(closeButton);
-
-    expect(container.firstChild).toHaveClass(
-      "invisible opacity-0 pointer-events-none"
+  it("should render modal hidden", () => {
+    render(
+      <Modal title="test" setShowModal={vi.fn()}>
+        content
+      </Modal>
     );
+
+    expect(screen.queryByText("test")).not.toBeInTheDocument();
+    expect(screen.queryByText("content")).not.toBeInTheDocument();
+    expect(screen.queryByLabelText("Close Modal")).not.toBeInTheDocument();
+  });
+
+  it("should render open and close modal", () => {
+    const setShowModal = vi.fn();
+
+    render(
+      <Modal title="test" showModal setShowModal={setShowModal}>
+        content
+      </Modal>
+    );
+
+    fireEvent.click(screen.getByLabelText("Close Modal"));
+
+    expect(setShowModal).toHaveBeenCalledTimes(1);
+    expect(setShowModal).toHaveBeenCalledWith(false);
   });
 });
