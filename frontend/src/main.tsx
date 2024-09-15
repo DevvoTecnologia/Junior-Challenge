557415ab4087866b64ac45e257c0e949dca7a250
Evandro Calado <evandrocalado07@gmail.com>
2024-09-15 17:01:49 -0300

    🐳 chore: add react hot toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Toaster toastOptions={{ duration: 3000 }} />
+  </StrictMode>
+);
