58cd4a9a16f9f9cb8153efc7e242c0f24bdc74d5
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-14 21:17:35 -0300

    feat(api): add seed script for initializing smiths data

---
+++ api/src/infra/seed.ts
@@ -0,0 +1,62 @@
+import { AppDataSource } from './data-source'
+import { nanoid } from 'nanoid'
+
+async function seed() {
+  const dataSource = AppDataSource
+  await dataSource.initialize()
+
+  const queryRunner = dataSource.createQueryRunner()
+  await queryRunner.connect()
+  await queryRunner.startTransaction()
+
+  try {
+    const smiths = [
+      {
+        name: 'Elfos Arcanos',
+        description:
+          'Mestres da magia e da criação. Seus artefatos possuem propriedades encantadas e misteriosas. São conhecidos por criar apenas até 3 anéis, todos com um poder inigualável.',
+        itemLimit: 3,
+      },
+      {
+        name: 'Anões Forjadores',
+        description:
+          'Especialistas em forjar artefatos robustos e duráveis. Seu conhecimento sobre metais é lendário, permitindo a criação de até 7 anéis com habilidades surpreendentes.',
+        itemLimit: 7,
+      },
+      {
+        name: 'Homens Valorosos',
+        description:
+          'Artífices destemidos com habilidades em criação de artefatos que refletem sua coragem e astúcia. Podem criar até 9 anéis com uma ampla gama de poderes.',
+        itemLimit: 9,
+      },
+      {
+        name: 'Sauron',
+        description:
+          'O Senhor das Trevas, conhecido por seu poder absoluto. Possui a habilidade de criar apenas um anel, mas este é capaz de controlar e dominar outros artefatos com uma força aterrorizante.',
+        itemLimit: 1,
+      },
+    ]
+
+    for (const smith of smiths) {
+      await queryRunner.query(
+        `INSERT INTO "smiths" (id, name, description, "itemLimit") VALUES ($1, $2, $3, $4)`,
+        [nanoid(), smith.name, smith.description, smith.itemLimit],
+      )
+    }
+
+    await queryRunner.commitTransaction()
+    console.log('Smiths seed data inserted successfully!')
+  } catch (error) {
+    await queryRunner.rollbackTransaction()
+    console.error('Error seeding data:', error)
+  } finally {
+    await queryRunner.release()
+  }
+
+  await dataSource.destroy()
+}
+
+seed().catch((error) => {
+  console.error('Error inserting data', error)
+  process.exit(1)
+})
