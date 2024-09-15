cd6079266c72f69d09f76bfce35d9df527b635eb
Evandro Calado <evandrocalado07@gmail.com>
2024-09-15 17:26:45 -0300

    🦄 refactor: divide logic between service and controller

--- backend/src/controllers/ringController.ts
+++ backend/src/controllers/ringController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Ring from "../models/Ring";
+import * as ringService from "../services/ringService";
 
 // create a ring
 export const createRing = async (req: Request, res: Response) => {
@@ -12,48 +12,26 @@ export const createRing = async (req: Request, res: Response) => {
         .json({ message: "All fields are required to create a ring." });
     }
 
-    const totalRing = await Ring.countDocuments({ forgedBy });
-    const maxRings: { [key: string]: number } = {
-      Elves: 3,
-      Dwarves: 7,
-      Men: 9,
-      Sauron: 1,
-    };
-
-    if (maxRings[forgedBy] !== undefined && totalRing >= maxRings[forgedBy]) {
-      return res.status(400).json({
-        message: `Maximum allowed rings forged by ${forgedBy} is ${maxRings[forgedBy]}`,
-      });
-    }
-
-    const newRing = new Ring({
-      name,
-      power,
-      holder,
-      forgedBy,
-      image,
-    });
-
-    await newRing.save();
+    const newRing = await ringService.createRing(req.body);
 
     res.status(201).json(newRing);
   } catch (error) {
-    res.status(500).json({ error: "Server error while creating the ring." });
+    if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 };
 
 // get all rings
 export const getRings = async (req: Request, res: Response) => {
   try {
-    const rings = await Ring.find();
-
-    if (!rings) {
-      return res.status(404).json({ message: "No rings found." });
-    }
+    const rings = await ringService.getRings();
 
     res.status(200).json(rings);
   } catch (error) {
-    res.status(500).json({ error: "Server error while getting the rings." });
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 };
 
@@ -68,9 +46,7 @@ export const UpdateRing = async (req: Request, res: Response) => {
         .json({ message: "All fields are required to update the ring." });
     }
 
-    const updatedRing = await Ring.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
+    const updatedRing = await ringService.updateRing(req.params.id, req.body);
 
     if (!updatedRing) {
       return res.status(404).json({ message: "Ring not found." });
@@ -78,21 +54,21 @@ export const UpdateRing = async (req: Request, res: Response) => {
 
     res.status(200).json(updatedRing);
   } catch (error) {
-    res.status(500).json({ error: "Server error while updating the ring." });
+    if (error instanceof Error) {
+      res.status(404).json({ error: error.message });
+    }
   }
 };
 
 // delete a ring
 export const deleteRing = async (req: Request, res: Response) => {
   try {
-    const deletedRing = await Ring.findByIdAndDelete(req.params.id);
-
-    if (!deletedRing) {
-      return res.status(404).json({ message: "Ring not found." });
-    }
+    const deletedRing = await ringService.deleteRing(req.params.id);
 
     res.status(200).json(deletedRing);
   } catch (error) {
-    res.status(500).json({ error: "Server error while deleting the ring." });
+    if (error instanceof Error) {
+      res.status(404).json({ error: error.message });
+    }
   }
 };
