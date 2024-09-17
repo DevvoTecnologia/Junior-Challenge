154b201753cb63bbbfd86be54f47b41db0ebda03
ederbiason <ederbiason@hotmail.com>
2024-09-17 18:52:40 -0300

    Desafio finalizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { RingsCarousel } from "@/components/RingsCarousel";
 
 export function App() {
-  
+  /* Desafio finalizado */
+
   return (
     <div className="mx-auto w-full h-screen flex justify-center items-center">
       <RingsCarousel />
