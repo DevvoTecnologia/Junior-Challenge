ded1e08a342cc85278b28df9d8ef8497aa924fe0
= <=>
2024-09-18 16:53:10 -0300

    fix: fixed wrong 404 with redirect

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/auth",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
