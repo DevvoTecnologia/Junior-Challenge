9a8eb660749d3cb32f95808a20eb50d8182726f1
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-25 15:19:52 -0300

    chore: initial commit and config prettier and eslint

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
