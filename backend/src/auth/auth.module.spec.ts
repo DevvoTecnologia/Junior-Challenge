f80d3f369a09b36272b7079778384d7f70c5cc9d
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-28 03:27:03 -0300

    update modules tests and remove no necessary configs swagger

---
+++ backend/src/auth/auth.module.spec.ts
@@ -0,0 +1,7 @@
+import { AuthModule } from "./auth.module";
+
+describe("AuthModule", () => {
+  it("should be defined", () => {
+    expect(AuthModule).toBeDefined();
+  });
+});
---
+++ backend/src/ring/ring.module.spec.ts
@@ -0,0 +1,7 @@
+import { RingModule } from "./ring.module";
+
+describe("RingModule", () => {
+  it("should be defined", () => {
+    expect(RingModule).toBeDefined();
+  });
+});
