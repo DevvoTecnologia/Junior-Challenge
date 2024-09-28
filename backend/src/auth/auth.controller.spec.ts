e52c37a3cf81173a867bc0d4a031c2b7a92d94df
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-28 03:14:34 -0300

    test: create auth service, guard and controller tests

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -9,6 +9,18 @@ import { AuthService } from "./auth.service";
 describe("AuthController", () => {
   let controller: AuthController;
 
+  const mockAuthService = {
+    signIn: jest.fn().mockResolvedValue({
+      accessToken: "asdX0.hF60cVqQ2LSkEA1dkwXUZpPLasd6b5DnL1lw",
+      userId: 1,
+      username: "admin",
+    }),
+    test: jest.fn().mockResolvedValue({
+      sub: 1,
+      username: "admin",
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [JwtModule, ConfigModule],
@@ -16,7 +28,7 @@ describe("AuthController", () => {
       providers: [AuthService],
     })
       .overrideProvider(AuthService)
-      .useValue({})
+      .useValue(mockAuthService)
       .compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -24,5 +36,38 @@ describe("AuthController", () => {
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("signIn", () => {
+    it("should return an object with accessToken, userId, and username", async () => {
+      const authDto = {
+        username: "admin",
+        password: "password",
+      };
+
+      const response = await controller.signIn(authDto);
+
+      expect(response).toEqual({
+        accessToken: "asdX0.hF60cVqQ2LSkEA1dkwXUZpPLasd6b5DnL1lw",
+        userId: 1,
+        username: "admin",
+      });
+    });
+  });
+
+  describe("getProfile", () => {
+    it("should return an object with sub and username", async () => {
+      const response = await controller.getProfile({
+        user: {
+          sub: 1,
+          username: "admin",
+        },
+      });
+
+      expect(response).toEqual({
+        sub: 1,
+        username: "admin",
+      });
+    });
   });
 });
--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -8,13 +8,28 @@ import { AuthService } from "./auth.service";
 
 describe("AuthService", () => {
   let service: AuthService;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let userModel: typeof User;
+
+  const mockUserModel = {
+    findOne: jest.fn().mockResolvedValue({
+      id: 1,
+      username: "admin",
+      passwordIsValid: jest.fn(),
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [JwtModule],
-      providers: [AuthService, { provide: getModelToken(User), useValue: {} }],
+      imports: [
+        JwtModule.register({
+          secret: "secret",
+          signOptions: { expiresIn: "60s" },
+        }),
+      ],
+      providers: [
+        AuthService,
+        { provide: getModelToken(User), useValue: mockUserModel },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -23,5 +38,58 @@ describe("AuthService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("signIn", () => {
+    it("should throw UnauthorizedException if user does not exist", async () => {
+      jest.spyOn(userModel, "findOne").mockResolvedValue(null);
+
+      await expect(
+        service.signIn({
+          username: "admin",
+          password: "password",
+        }),
+      ).rejects.toThrow("User or password incorrect");
+    });
+
+    it("should throw UnauthorizedException if password is invalid", async () => {
+      const mockUser = {
+        passwordIsValid: jest.fn().mockResolvedValue(false),
+      };
+
+      jest
+        .spyOn(userModel, "findOne")
+        .mockResolvedValue(mockUser as unknown as User);
+
+      await expect(
+        service.signIn({
+          username: "admin",
+          password: "password",
+        }),
+      ).rejects.toThrow("User or password incorrect");
+    });
+
+    it("should return an object with accessToken, userId and username", async () => {
+      const mockUser = {
+        id: 1,
+        username: "admin",
+        passwordIsValid: jest.fn().mockResolvedValue(true),
+      };
+
+      jest
+        .spyOn(userModel, "findOne")
+        .mockResolvedValue(mockUser as unknown as User);
+
+      const response = await service.signIn({
+        username: "admin",
+        password: "password",
+      });
+
+      expect(response).toEqual({
+        accessToken: expect.any(String),
+        userId: 1,
+        username: "admin",
+      });
+    });
   });
 });
