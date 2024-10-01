dec1d55f0e6b4ea6975aa9baeed47f94ce0f26de
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-10-01 12:45:08 -0300

    Desafio finalizado

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: process.env.NODE_ENV !== "production" ? "http" : "https",
-                hostname: process.env.BASE_HOST || "192.168.100.3",
+                hostname: process.env.API_BASE_HOST || "192.168.100.3",
                 port: process.env.NODE_ENV !== "production" ? "3000" : undefined,
             }
         ]
