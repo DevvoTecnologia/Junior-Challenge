a0791467130899eab95ba3b282accbc86630f6ee
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-10-08 13:15:48 -0300

    test: covering 100%

--- backend/src/auth/auth.module.spec.ts
+++ backend/src/auth/auth.module.spec.ts
@@ -1,7 +1,23 @@
+import { JwtModule } from "@nestjs/jwt";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Test, type TestingModule } from "@nestjs/testing";
+
 import { AuthModule } from "./auth.module";
 
 describe("AuthModule", () => {
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [AuthModule, SequelizeModule.forRoot({ dialect: "sqlite" })],
+    }).compile();
+  });
+
   it("should be defined", () => {
-    expect(AuthModule).toBeDefined();
+    expect(module).toBeDefined();
+  });
+
+  it("should JwtModule be defined", () => {
+    expect(module.get(JwtModule)).toBeDefined();
   });
 });
